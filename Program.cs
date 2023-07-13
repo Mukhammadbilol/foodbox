ffbe22df3a89e9f04ccf0a7f542b15d38712462b
Mukhammadbilol <muhammadbilolumaroff@gmail.com>
2023-07-13 15:51:02 +0500

    started update handler

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 using FoodBox.Services;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddTransient<IUpdateHandler, UpdateHandler>();
 builder.Services.AddHostedService<BotStartingBackgroundService>();
 builder.Services.AddSingleton<ITelegramBotClient, TelegramBotClient>(
     p => new TelegramBotClient(builder.Configuration.GetValue("BotApiKey", string.Empty)));
